bd736f3bb9a91c7f7bb2c07c2ae885495747f81b
veronica <veronica@blazhko.tech>
2019-11-08 13:36:01 +0200

    init

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/blzhk/golang-tg-bot
 
 go 1.12
 
-require gopkg.in/tucnak/telebot.v2 v2.0.0-20191005061224-d0707a9d73c4 // indirect
+require gopkg.in/tucnak/telebot.v2 v2.0.0-20191005061224-d0707a9d73c4
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	var (
+		port = os.Getenv("PORT")
+		publicURL = os.Getenv("PUBLIC_URL")
+		token = os.Getenv("TOKEN")
+	)
+
+	webhook := &tb.Webhook{
+		Listen: ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL:publicURL},
+	}
+
+	pref := tb.Settings{
+		Token:    token,
+		Poller:   webhook,
+	}
+
+	bot, err := tb.NewBot(pref)
+	if  err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Handle("/hello", func(m *tb.Message) {
+		bot.Send(m.Sender, "Hi!")
+	})
+}
