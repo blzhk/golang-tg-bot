9119df67331a7ee859ed54c7ffe02aa5ac4160dc
veronica <veronica@blazhko.tech>
2019-11-11 09:18:20 +0200

    logs file

--- main.go
+++ main.go
@@ -3,32 +3,41 @@ package main
 import (
 	tb "gopkg.in/tucnak/telebot.v2"
 	"log"
+	"os"
 )
 
 func main() {
+	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Println("This is a test log entry")
 	var (
-		//port      = os.Getenv("PORT")
-		//publicURL = os.Getenv("PUBLIC_URL")
-		token = "810595026:AAFZ2BcIbEOnPw9eyAIkoDSd2VPcJRyRnZ4"
+		port      = os.Getenv("PORT")
+		publicURL = os.Getenv("PUBLIC_URL")
+		token     = "810595026:AAFZ2BcIbEOnPw9eyAIkoDSd2VPcJRyRnZ4"
 	)
 
-	//webhook := &tb.Webhook{
-	//	Listen:   ":" + port,
-	//	Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
-	//}
-
-	pref := tb.Settings{
-		Token: token,
-		//Poller: webhook,
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
 	}
 
-	_, err := tb.NewBot(pref)
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+
+	b, err := tb.NewBot(pref)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//b.Handle("/hello", func(m *tb.Message) {
-	//	b.Send(m.Sender, "Hi!")
-	//})
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hi!")
+	})
 
 }
